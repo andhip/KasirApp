c3a8c49e8d5fab4df32b1c72dfbae79ee279fa9f
andhip <andhi.puspianto10@gmail.com>
2021-03-20 12:12:26 +0700

    add components, navbar and titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Row, Col } from "react-bootstrap";
 import "./App.css";
-import NavComponent from "./components/NavComponent";
+import { Hasil, ListCategories, NavComponent } from "./components";
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
+      <div className="mt-3">
+        <div className="container-fluid">
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Product</strong>
+              </h4>
+              <hr></hr>
+            </Col>
+            <Hasil />
+          </Row>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Hasil from "./Hasil";
+import ListCategories from "./ListCategories";
+import NavComponent from "./NavComponent";
+
+export { Hasil, ListCategories, NavComponent };
